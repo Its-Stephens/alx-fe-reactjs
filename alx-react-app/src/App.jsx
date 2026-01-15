5f156bba4df5ac7181981995eb74dff3b437ae65
Stephen <okafors261@gmail.com>
2026-01-15 19:28:43 +0100

    alx task

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,6 +1,6 @@
-import Header from "./Header.jsx";
-import MainContent from "./MainContent.jsx";
-import Footer from "./Footer.jsx"; 
+import Header from './Header.jsx';
+import MainContent from './MainContent.jsx';
+import Footer from './Footer.jsx'; 
 
 function App() {
  
