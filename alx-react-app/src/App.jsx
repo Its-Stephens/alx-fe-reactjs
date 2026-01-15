d639e350ea61ef27f11888d1ed553db2e38a67e6
Stephen <okafors261@gmail.com>
2026-01-16 00:11:19 +0100

    completed task

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,17 +1,17 @@
-import Header from './Header.jsx';
-import MainContent from './MainContent.jsx';
-import Footer from './Footer.jsx'; 
+import UserProfile from "./components/UserProfile.jsx";
+import Header from "./Header.jsx";
+import MainContent from "./MainContent.jsx";
+import Footer from "./Footer.jsx";
 
 function App() {
- 
-
   return (
     <>
+      <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
       <Header />
       <MainContent />
       <Footer />
     </>
-  )
+  );
 }
 
 export default App;
