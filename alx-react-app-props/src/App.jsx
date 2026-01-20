a75293d0b1c00caf373188f6e75f0163d834bb03
Stephen <okafors261@gmail.com>
2026-01-20 23:24:24 +0100

    captilization fixed

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -4,9 +4,9 @@ import UserContext from "../src/components/UserContext";
 function App() {
   const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
 
-  <UserContext.provider value={userData}>
+  <UserContext.Provider value={userData}>
     return <ProfilePage userData={userData} />;
-  </UserContext.provider>;
+  </UserContext.Provider>;
 }
 
 export default App;
