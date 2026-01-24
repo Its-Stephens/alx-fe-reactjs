1cd95df7fa23cf7cb309cbde62a37e18267bbc97
Stephen <okafors261@gmail.com>
2026-01-24 20:34:21 +0100

    react task

---
+++ recipe-sharing-app/src/App.jsx
@@ -0,0 +1,12 @@
+import AddRecipeForm from "./components/AddRecipeForm";
+import RecipeList from "./components/RecipeList";
+
+
+function App() {
+  <>
+    <AddRecipeForm />
+    <RecipeList />
+  </>;
+}
+
+export default App
